a784b4ab9679cdb7b9e6ada1a1525c65c509483a
Tim Proffitt <tim.proffitt@gmail.com>
2013-12-11 11:45:54 -0600

    signin complete - up to 8.2.5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		# login the user and go to the profile page
-  		#login_user
+  		sign_in(user)
   		redirect_to user_path(user)
   	else
   		flash.now[:error] = "auth failed"
