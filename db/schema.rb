b465d0bc23619bfd552a9cee3299e14b2a54c545
Tim Proffitt <tim.proffitt@gmail.com>
2013-12-27 16:18:57 -0600

    something changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131226174657) do
+ActiveRecord::Schema.define(version: 20131227172237) do
+
+# Could not dump table "sites" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "users", force: true do |t|
     t.string   "name"
