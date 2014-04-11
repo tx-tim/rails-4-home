8601de169ca15656befb76022d004035a982ee9d
Tim Proffitt <tim.proffitt@gmail.com>
2014-04-11 09:58:38 -0500

    fixed js for ie8

--- app/assets/javascripts/caredes/email-signup.js
+++ app/assets/javascripts/caredes/email-signup.js
@@ -1,33 +1,21 @@
-console.log("email-signup");
+//console.log("email-signup");
 
 $(function(){
     if( $('#email-signup-submit').length ){
-        console.log("email element found");
 
-        $('#email-signup-submit').click(function(){
-            console.log(Modernizr.mq('(min-width: 991px)') )
+        $('#email-signup-submit').click(function(event){
 
-            if( Modernizr.mq('(min-width: 991px)' ) ){
-                console.log("drop a modal");
-                $('.offcanvas-email-form').modal();
-                return false; 
-            }
-            else{
-                console.log("try a slide thing");
-                // $('.offcanvas-email-form')
-                //.removeClass('modal')
-                // .removeClass('fade')
-                // .detach()
-                // .appendTo('.email-signup')
-                // .show();
-                //$('.offcanvas-email-form .modal-content')
-                //.detach()
-                //.prependTo('.footer-brands-section')
-                //.show();
-            }
-
-
-           
+            if( Modernizr.mq('(min-width: 991px)') || document.body.clientWidth >= 991  ){
+                try{
+                    $('.offcanvas-email-form').modal();
+                    return false; 
+                }
+                catch(ex){
+                    alert(ex);
+                    return false;
+                }
+                
+            }           
         });
     }
 });
